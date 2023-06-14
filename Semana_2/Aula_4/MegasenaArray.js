9523f8c7824e93d846991487030ba6906cf5183c
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-06-14 13:14:11 -0300

    commit

---
+++ Semana_2/Aula_4/MegasenaArray.js
@@ -0,0 +1,5 @@
+var megaSena = [60, 59, 30, 12, 3, 4]; // 6 posições, 5 indices
+
+for (var contador = 0; contador < megaSena.length; contador++) {
+  console.log("O número sorteado foi " + megaSena[contador]);
+}
