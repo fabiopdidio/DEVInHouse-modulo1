59945f5d1dd9c2ab5a802fa070d924f06706cf0e
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-07-24 18:45:07 -0300

    update

--- Semana_2/Aula_1/CalculoFGTS.js
+++ Semana_2/Aula_1/CalculoFGTS.js
@@ -1,10 +1,9 @@
 /*
 Cálculo do FGTS baseado no salário bruto recebido
 */
-
-let salarioBruto = Number(prompt("Digite seu salário"));
-let resultado = salarioBruto * 0.08;
-let resultadoAnual = resultado * 12;
+const salarioBruto = Number(prompt("Digite seu salário"));
+const resultado = salarioBruto * 0.08;
+const resultadoAnual = resultado * 12;
 
 console.log("Seu FGTS é" + resultado);
 console.log("No ano você receberá: " + resultadoAnual);
