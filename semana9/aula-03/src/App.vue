16e4c42c3f3ee0c38ef3e00d773bd98d4a76d409
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-03 12:50:21 -0300

    update

---
+++ semana9/aula-03/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- utilizando o componente -->
+  <Card>
+    <template v-slot:header>
+      <h1>Titulo do meu card</h1>
+    </template>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, rem
+      harum debitis animi ducimus expedita numquam autem nostrum, voluptatum
+      suscipit dolorum unde vel eaque dolorem doloremque sunt. Ea, sit dicta.
+    </p>
+    <template #footer>
+      <h6>Created by Fábio Didio</h6>
+    </template>
+  </Card>
+</template>
+
+<script>
+// importando componente
+import Card from "./components/Card.vue";
+
+export default {
+  // registrando componente
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ semana9/aula-03/src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+</template>
+
+<script></script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
