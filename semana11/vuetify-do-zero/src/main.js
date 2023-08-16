65503200b7ebe8b86b90160f01cb59571a57bb80
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-16 19:37:24 -0300

    Update main.js

--- semana11/vuetify-do-zero/src/main.js
+++ semana11/vuetify-do-zero/src/main.js
@@ -2,12 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Vuetify import
-import 'vuetify/styles' 
-import '@mdi/font/css/materialdesignicons.css' 
-import { createVuetify } from 'vuetify' 
-import * as components from 'vuetify/components' 
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({ components, directives, icons: { defaultSet: 'mdi' }, })
+const vuetify = createVuetify({ components, directives, icons: { defaultSet: 'mdi' } })
 
 createApp(App).use(vuetify).mount('#app')
