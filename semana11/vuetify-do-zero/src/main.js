5de9faad3477b7a8b482c1725aa88b6038f616bc
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-16 19:36:20 -0300

    update

---
+++ semana11/vuetify-do-zero/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Vuetify import
+import 'vuetify/styles' 
+import '@mdi/font/css/materialdesignicons.css' 
+import { createVuetify } from 'vuetify' 
+import * as components from 'vuetify/components' 
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({ components, directives, icons: { defaultSet: 'mdi' }, })
+
+createApp(App).use(vuetify).mount('#app')
