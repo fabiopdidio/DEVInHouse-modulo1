cb8774308fc8cf547eb8ee97d9b939b5251e5b5a
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-18 09:54:00 -0300

    update

---
+++ semana11/ExercicioM1S09/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <img
+      class="logo"
+      src="https://d6ce0no7ktiq.cloudfront.net/images/preview/2020/03/03/design-61468/template-sticker-600x600.png"
+      alt="Logo da Empresa"
+    />
+    <h1>Farmácia Z</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {},
+};
+</script>
+
+<style>
+.header {
+  display: flex;
+  align-items: center;
+  padding: 10px 2em;
+  width: 100vw;
+  background-color: #e2e2e2;
+}
+
+.logo {
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+}
+</style>
